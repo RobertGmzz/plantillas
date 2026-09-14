2b67712f3aea9abc03fcf78d7f7b80343ef206ed
RobertGmzz <r0b3rt0gzz@gmail.com>
2026-09-14 02:05:41 -0300

    first commit, the hero section

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { BurguerLanding } from "./burguer-landing/burguer-landing"
+
+function App() {
+
+  return (
+    <BurguerLanding />
+  )
+}
+
+export default App
---
+++ src/burguer-landing/burguer-landing.jsx
@@ -0,0 +1,12 @@
+import { Hero } from "./sections/hero"
+
+export function BurguerLanding() {
+    return (
+        <div className="relative flex flex-col justify-center w-full h-auto paper-bg">
+            <Hero />
+            <section id="menu" className="w-full h-screen bg-orange-900">
+                a
+            </section>
+        </div>
+    )
+}
